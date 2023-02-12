794086613e85783b4ea62808e41d7b43c7e52437
Jayanta <bjayanta.me@gmail.com>
2023-02-12 20:18:05 +0600

    add readme

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
-# WP Plugin Development
+<?php
 
-## Setup
+/*
+    Plugin name: Our Word Filter Plugin
+    Description: Repace a list of words.
+    version: 1.0
+    Author: Jayanta Biswas
+    Author URI: http://jayanta.me
+*/
 
-  - Plugin name: Our Word Filter Plugin
-  - Description: Repace a list of words.
-  - version: 1.0
-  - Author: Jayanta Biswas
-  - Author URI: http://jayanta.me
-  
-## Restrict direct access
-
-```php
 if(! defined('ABSPATH')) exit; // Exit if accessed directly
-```
 
-## Create menu
-
-```php
 class OurWordFilterPlugin {
     function __construct() {
         add_action('admin_menu', [$this, 'ourMenu']);
@@ -25,12 +18,11 @@ class OurWordFilterPlugin {
     function ourMenu() {
         // Create main menu
         add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], 'dashicons-smiley', 100);
-		
         // Create submenu page
         add_submenu_page('ourwordfilter', 'Word To Filter', 'Words List', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage']);
         add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', [$this, 'optionsSubPage']);
     }
-	
+
     function wordFilterPage() { ?>
         Hello world.
     <?php }
@@ -41,12 +33,3 @@ class OurWordFilterPlugin {
 }
 
 $ourWordFilterPlugin = new OurWordFilterPlugin();
-```
-
-## Noticeable
-
-| Function name | Description |
-| ------------- | ----------- |
-| add_action | When specific events occur. In this case 'admin_menu' |
-| add_menu_page | To create main menu |
-| add_submenu_page | To create submenu |
