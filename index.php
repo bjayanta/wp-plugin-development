5244c789854303f2c1d076f4936f5c60a0dbeeee
Jayanta <bjayanta.me@gmail.com>
2023-02-12 21:10:53 +0600

    add custom stylesheet

--- index.php
+++ index.php
@@ -17,15 +17,29 @@ class OurWordFilterPlugin {
 
     function ourMenu() {
         // Create main menu
-        // add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgZmlsbD0iY3VycmVudENvbG9yIiBjbGFzcz0iYmkgYmktZnVubmVsIiB2aWV3Qm94PSIwIDAgMTYgMTYiPgogIDxwYXRoIGQ9Ik0xLjUgMS41QS41LjUgMCAwIDEgMiAxaDEyYS41LjUgMCAwIDEgLjUuNXYyYS41LjUgMCAwIDEtLjEyOC4zMzRMMTAgOC42OTJWMTMuNWEuNS41IDAgMCAxLS4zNDIuNDc0bC0zIDFBLjUuNSAwIDAgMSA2IDE0LjVWOC42OTJMMS42MjggMy44MzRBLjUuNSAwIDAgMSAxLjUgMy41di0yem0xIC41djEuMzA4bDQuMzcyIDQuODU4QS41LjUgMCAwIDEgNyA4LjV2NS4zMDZsMi0uNjY2VjguNWEuNS41IDAgMCAxIC4xMjgtLjMzNEwxMy41IDMuMzA4VjJoLTExeiIvPgo8L3N2Zz4=', 100);
-        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], plugin_dir_url(__FILE__) . 'filter.svg', 100);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgZmlsbD0iY3VycmVudENvbG9yIiBjbGFzcz0iYmkgYmktZnVubmVsIiB2aWV3Qm94PSIwIDAgMTYgMTYiPgogIDxwYXRoIGQ9Ik0xLjUgMS41QS41LjUgMCAwIDEgMiAxaDEyYS41LjUgMCAwIDEgLjUuNXYyYS41LjUgMCAwIDEtLjEyOC4zMzRMMTAgOC42OTJWMTMuNWEuNS41IDAgMCAxLS4zNDIuNDc0bC0zIDFBLjUuNSAwIDAgMSA2IDE0LjVWOC42OTJMMS42MjggMy44MzRBLjUuNSAwIDAgMSAxLjUgMy41di0yem0xIC41djEuMzA4bDQuMzcyIDQuODU4QS41LjUgMCAwIDEgNyA4LjV2NS4zMDZsMi0uNjY2VjguNWEuNS41IDAgMCAxIC4xMjgtLjMzNEwxMy41IDMuMzA4VjJoLTExeiIvPgo8L3N2Zz4=', 100);
         // Create submenu page
         add_submenu_page('ourwordfilter', 'Word To Filter', 'Words List', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage']);
         add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', [$this, 'optionsSubPage']);
+        add_action("load-{$mainPageHook}", [$this, 'mainPageAssets']);
+    }
+
+    function mainPageAssets() {
+        wp_enqueue_style('filterAdminCSS', plugin_dir_url(__FILE__) . 'styles.css');
     }
 
     function wordFilterPage() { ?>
-        Hello world.
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <form method="post">
+                <label for="plugin_words_to_filter">Enter a <strong>comma(,) seperated</strong> list of words to filter from your site's content.</label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"></textarea>
+                </div>
+
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
     <?php }
 
     function optionsSubPage() { ?>
--- readme.md
+++ readme.md
@@ -1,19 +1,26 @@
-# Custom Menu Icon
+# Custom CSS
 
-** Method 1: Convert SVG icon to binary format **
-
-1. Open Google Chrome dev tool
-2. Type btoa(`YOUR-SVG-CODE`) and hit enter 
+## Code
 
 ```php
-add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgZmlsbD0iY3VycmVudENvbG9yIiBjbGFzcz0iYmkgYmktZnVubmVsIiB2aWV3Qm94PSIwIDAgMTYgMTYiPgogIDxwYXRoIGQ9Ik0xLjUgMS41QS41LjUgMCAwIDEgMiAxaDEyYS41LjUgMCAwIDEgLjUuNXYyYS41LjUgMCAwIDEtLjEyOC4zMzRMMTAgOC42OTJWMTMuNWEuNS41IDAgMCAxLS4zNDIuNDc0bC0zIDFBLjUuNSAwIDAgMSA2IDE0LjVWOC42OTJMMS42MjggMy44MzRBLjUuNSAwIDAgMSAxLjUgMy41di0yem0xIC41djEuMzA4bDQuMzcyIDQuODU4QS41LjUgMCAwIDEgNyA4LjV2NS4zMDZsMi0uNjY2VjguNWEuNS41IDAgMCAxIC4xMjgtLjMzNEwxMy41IDMuMzA4VjJoLTExeiIvPgo8L3N2Zz4=', 100);
+$mainPageHook = add_menu_page( 
+    string $page_title, 
+    string $menu_title, 
+    string $capability, 
+    string $menu_slug, 
+    callable $callback = '', 
+    string $icon_url = '', 
+    int|float $position = null);
+add_action("load-{$mainPageHook}", [$this, 'mainPageAssets']);
+
+function mainPageAssets() {
+    wp_enqueue_style('filterAdminCSS', plugin_dir_url(__FILE__) . 'styles.css');
+}
 ```
 
+## Noticeable
 
-** Method 2: Use SVG icon directly **
-
-Icon size 20 X 20
-
-```php
-add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', [$this, 'wordFilterPage'], plugin_dir_url(__FILE__) . 'filter.svg', 100);
-```
+| Function name | Description |
+| ------------- | ----------- |
+| load-{$mainPageHook} | Load when the hook execute |
+| wp_enqueue_style | Add stylesheet |
