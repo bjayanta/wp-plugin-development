c17caf62cc8219fcf006db8add1e8a9d74633690
Jayanta <bjayanta.me@gmail.com>
2023-02-15 16:28:08 +0600

    update php file

--- index.php
+++ index.php
@@ -14,11 +14,23 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class AreYouPayingAttention {
 
     function __construct() {
-        add_action('enqueue_block_editor_assets', [$this, 'adminAssets']);
+        // add_action('enqueue_block_editor_assets', [$this, 'adminAssets']);
+        add_action('init', [$this, 'adminAssets']);
     }
 
     function adminAssets() {
-        wp_enqueue_script('outnewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element']);
+        // wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element']);
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element']);
+        register_block_type('ourplugin/are-you-paying-attention', [
+            'editor_script' => 'ournewblocktype',
+            'render_callback' => [$this, 'theHTML']
+        ]);
+    }
+
+    function theHTML($attributes, $content) {
+        ob_start(); ?>
+        <h3>Today the sky is <?php echo esc_html($attributes['skyColor']); ?> and the grass is <?php echo esc_html($attributes['grassColor']); ?> !!!</h3>
+        <?php return ob_get_clean();
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     icon: "smiley",
     category: "common",
     attributes: {
-        skyColor: {type: "string", source: "text", selector: ".skyColor"},
-        grassColor: {type: "string", source: "text", selector: ".grassColor"}
+        // skyColor: {type: "string", source: "text", selector: ".skyColor"},
+        // grassColor: {type: "string", source: "text", selector: ".grassColor"}
+        skyColor: {type: "string"},
+        grassColor: {type: "string"}
     },
     edit: function(props) {
         function updateSkyColor(event) {
@@ -24,22 +26,51 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
             <div>
                 {/* <p>Hello, this is a paragraph.</p>
                 <h4>Hi there.</h4> */}
-
+ 
                 <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
                 <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
             </div>
         );
     },
     save: function(props) {
+        // Way: 1
         // return wp.element.createElement("h1", null, "This is the frontend");
 
-        return (
-            // <>
-            //     <h3>H3 on the frontend.</h3>
-            //     <h5>H5 on the frontend.</h5>
-            // </>
+        // Way: 2
+        // return (
+        //     <>
+        //         <h3>H3 on the frontend.</h3>
+        //         <h5>H5 on the frontend.</h5>
+        //     </>
+        // );
 
-            <h3>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</h3>
-        );
-    }
+        // Way: 3
+        // return (
+        //     <h6>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</h6>
+        // );
+
+        // Way: 4
+        return null;
+    },
+    // deprecated: [{
+    //     attributes: {
+    //         skyColor: {type: "string", source: "text", selector: ".skyColor"},
+    //         grassColor: {type: "string", source: "text", selector: ".grassColor"}
+    //     },
+    //     save: function(props) {
+    //         return (
+    //             <h3>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</h3>
+    //         );
+    //     }
+    // },{
+    //     attributes: {
+    //         skyColor: {type: "string", source: "text", selector: ".skyColor"},
+    //         grassColor: {type: "string", source: "text", selector: ".grassColor"}
+    //     },
+    //     save: function(props) {
+    //         return (
+    //             <p>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</p>
+    //         );
+    //     }
+    // }]
 });
