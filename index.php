6cfd7726f1547b2d8477bb81ee1fa2390901b61b
Jayanta <bjayanta.me@gmail.com>
2023-02-15 17:44:50 +0600

    init branch

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Plugin Name: Are You Paying Attention Quize
+    Plugin Name: Multiple Choice Quize
     Description: Give your readers a multiple choice question.
     Version: 1.0
     Author: Jayanta
@@ -11,23 +11,24 @@
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // Blueprint or Class
-class AreYouPayingAttention {
+class MultipleChoiceQuize {
 
     function __construct() {
-        // add_action('enqueue_block_editor_assets', [$this, 'adminAssets']);
         add_action('init', [$this, 'adminAssets']);
     }
 
     function adminAssets() {
-        // wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element']);
-        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element']);
+        wp_register_style('quizeditcss', plugin_dir_url(__FILE__) . 'build/index.css');
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', ['wp-blocks', 'wp-element', 'wp-editor']);
+
         register_block_type('ourplugin/are-you-paying-attention', [
             'editor_script' => 'ournewblocktype',
+            'editor_style' => 'quizeditcss',
             'render_callback' => [$this, 'theHTML']
         ]);
     }
 
-    function theHTML($attributes, $content) {
+    function theHTML($attributes) {
         ob_start(); ?>
         <h3>Today the sky is <?php echo esc_html($attributes['skyColor']); ?> and the grass is <?php echo esc_html($attributes['grassColor']); ?> !!!</h3>
         <?php return ob_get_clean();
@@ -36,4 +37,4 @@ class AreYouPayingAttention {
 }
 
 // Object
-$areYouPayingAttention = new AreYouPayingAttention();
+$multipleChoiceQuize = new MultipleChoiceQuize();
--- src/index.js
+++ src/index.js
@@ -1,76 +1,51 @@
+import "./index.scss";
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from "@wordpress/components";
+
 wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     title: "Are You Paying Attention",
     icon: "smiley",
     category: "common",
     attributes: {
-        // skyColor: {type: "string", source: "text", selector: ".skyColor"},
-        // grassColor: {type: "string", source: "text", selector: ".grassColor"}
         skyColor: {type: "string"},
         grassColor: {type: "string"}
     },
-    edit: function(props) {
-        function updateSkyColor(event) {
-            props.setAttributes({skyColor: event.target.value});
-        }
-
-        function updateGrassColor(event) {
-            props.setAttributes({grassColor: event.target.value});
-        }
-
-        // return wp.element.createElement("div", null, [
-        //     wp.element.createElement("h1", null, "This is a h1."),
-        //     wp.element.createElement("h4", {style: {color: 'skyblue'}}, "Hello this is a h4."),
-        // ]);
-
-        return (
-            <div>
-                {/* <p>Hello, this is a paragraph.</p>
-                <h4>Hi there.</h4> */}
- 
-                <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-                <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
-            </div>
-        );
-    },
+    edit: EditComponent,
     save: function(props) {
-        // Way: 1
-        // return wp.element.createElement("h1", null, "This is the frontend");
-
-        // Way: 2
-        // return (
-        //     <>
-        //         <h3>H3 on the frontend.</h3>
-        //         <h5>H5 on the frontend.</h5>
-        //     </>
-        // );
-
-        // Way: 3
-        // return (
-        //     <h6>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</h6>
-        // );
-
-        // Way: 4
         return null;
-    },
-    // deprecated: [{
-    //     attributes: {
-    //         skyColor: {type: "string", source: "text", selector: ".skyColor"},
-    //         grassColor: {type: "string", source: "text", selector: ".grassColor"}
-    //     },
-    //     save: function(props) {
-    //         return (
-    //             <h3>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</h3>
-    //         );
-    //     }
-    // },{
-    //     attributes: {
-    //         skyColor: {type: "string", source: "text", selector: ".skyColor"},
-    //         grassColor: {type: "string", source: "text", selector: ".grassColor"}
-    //     },
-    //     save: function(props) {
-    //         return (
-    //             <p>Today the sky is <span className="skyColor">{ props.attributes.skyColor }</span> and the grass is <span className="grassColor">{ props.attributes.grassColor }</span>.</p>
-    //         );
-    //     }
-    // }]
+    }
 });
+
+// Component
+function EditComponent(props) {
+    function updateSkyColor(event) {
+        props.setAttributes({skyColor: event.target.value});
+    }
+
+    function updateGrassColor(event) {
+        props.setAttributes({grassColor: event.target.value});
+    }
+
+    return (
+        <div className="paying-attention-edit-block">
+            <TextControl label="Question:" />
+
+            <p>Answers:</p>
+
+            <Flex>
+                <FlexBlock>
+                    <TextControl />
+                </FlexBlock>
+
+                <FlexItem>
+                    <Button>
+                        <Icon className="mark-as-correct" icon="star-empty" />
+                    </Button>
+                </FlexItem>
+
+                <FlexItem>
+                    <Button>Delete</Button>
+                </FlexItem>
+            </Flex>
+        </div>
+    );
+}
